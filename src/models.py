1a75558e6aff8be7ad1c0f960f1cd0aae0db081a
Oussama3030 <59825001+Oussama3030@users.noreply.github.com>
2024-01-14 15:56:58 +0100

    Updated the model and a few plots

--- src/models.py
+++ src/models.py
@@ -1,19 +1,21 @@
 import torch
 import torch.nn as nn
 
-
-class Net_4_layers_v1(nn.Module):
+class Net_4_layers_v2(nn.Module):
     def __init__(self):
-        super(Net_4_layers_v1, self).__init__()
+        super(Net_4_layers_v2, self).__init__()
         self.fc1 = nn.Linear(6, 128)
+        self.bn1 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 256)
+        self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.fc4 = nn.Linear(128, 7)
         pass
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = torch.relu(self.bn1(self.fc1(x)))
+        x = torch.relu(self.bn2(self.fc2(x)))
+        x = torch.relu(self.bn3(self.fc3(x)))
+        x = torch.softmax(self.fc4(x), dim=1)
         return x
