f5bc8e0591906c8c7da43d704a530de488966546
Sean Wang <seanwang313@outlook.com>
2024-01-16 14:56:26 +0100

    Change linear to softmax.

--- src/models.py
+++ src/models.py
@@ -17,3 +17,20 @@ class Net_4_layers_v1(nn.Module):
         x = torch.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+
+
+class Net_4_layers_with_softmax(nn.Module):
+    def __init__(self):
+        super(Net_4_layers_with_softmax, self).__init__()
+        self.fc1 = nn.Linear(6, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 7)
+        pass
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.softmax(self.fc4(x), dim=1)
+        return x
