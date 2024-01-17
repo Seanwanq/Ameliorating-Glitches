dee688fa71e86f69f005f28ca6c6fa3da7f83b61
Oussama3030 <59825001+Oussama3030@users.noreply.github.com>
2024-01-17 22:34:59 +0100

    added testing

--- src/models.py
+++ src/models.py
@@ -32,5 +32,25 @@ class Net_4_layers_with_softmax(nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = torch.softmax(self.fc4(x), dim=1)
+        x = self.fc4(x)
+        x = torch.softmax(x, dim=1)
+
+        return x
+
+
+class Net_4_layers_with_relu(nn.Module):
+    def __init__(self):
+        super(Net_4_layers_with_relu, self).__init__()
+        self.fc1 = nn.Linear(6, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 7)
+        pass
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+
         return x
